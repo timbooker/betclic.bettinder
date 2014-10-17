47c537b503c7d096cd5689260fb9241bf038eba3
Tim Booker <booker.tim@gmail.com>
2014-10-17 15:44:38 +0200

    Win phone added

---
+++ BetClic.BetTinder.WindowsPhone/Views/FirstView.xaml.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <summary>
+//    Defines the FirstView.xaml type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BetClic.BetTinder.WindowsPhone.Views
+{
+    /// <summary>
+    ///    Defines the FirstView.xaml type.
+    /// </summary>
+    public partial class FirstView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstView"/> class.
+        /// </summary>
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
