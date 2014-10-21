9787074d179fb3ca5be5de8677f055ac818ca0b0
Tim Booker <booker.tim@gmail.com>
2014-10-21 14:49:35 +0200

    Added new list of bet historic results

--- BetClic.BetTinder.Core/Services/ProposedBetsService.cs
+++ BetClic.BetTinder.Core/Services/ProposedBetsService.cs
@@ -19,7 +19,29 @@ namespace BetClic.BetTinder.Core.Services
                 Name = Guid.NewGuid().ToString().Substring(0, 6),
                 Odds = new Random().NextDouble(),
                 ImageName = imageNames[new Random().Next(0, 5)] + ".jpg",
+                PreviousResults = GetHistoricalEvents()
             };
         }
+
+        private IEnumerable<PreviousResults> GetHistoricalEvents()
+        {
+            List<PreviousResults> list = new List<PreviousResults>();
+            Random rnd = new Random();
+            int numberOfLoops = rnd.Next(1, 10);
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                list.Add(new PreviousResults()
+                {
+                    HomeTeam = "Home Team " + Guid.NewGuid().ToString().Substring(0, 6),
+                    AwayTeam = "Away Team " + Guid.NewGuid().ToString().Substring(0, 6),
+                    AwayTeamScore = rnd.Next(0, 10),
+                    HomeTeamScore = rnd.Next(0, 10),
+                    MatchDate = new DateTime(rnd.Next(1960, 2014), rnd.Next(1,12), rnd.Next(1,28))
+                });
+            }
+
+            return list;
+        }
+
     }
 }
