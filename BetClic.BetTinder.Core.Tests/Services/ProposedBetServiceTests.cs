a26c7b65af427d904055589f6dcb74bebfaacbff
driznar <driznar+github@gmail.com>
2014-10-21 13:18:08 +0200

    Added a unit test for image path setting

--- BetClic.BetTinder.Core.Tests/Services/ProposedBetServiceTests.cs
+++ BetClic.BetTinder.Core.Tests/Services/ProposedBetServiceTests.cs
@@ -7,17 +7,33 @@ namespace BetClic.BetTinder.Core.Tests
     [TestFixture]
     public class ProposedBetServiceTests
     {
+        ProposedBetsService service = new ProposedBetsService();
+
         [Test]
         public void given_request_for_data_returns_something()
         {
-            // arrange 
-            var proposedBetService = new ProposedBetsService();
-
             // act
-            var result = proposedBetService.GetNextBet();
+            var result = service.GetNextBet();
 
             // then 
             Assert.That(result.Name, Is.Not.Empty);
+        }
+
+        [Test]
+        public void image_path_correctly_set()
+        {
+            int i = 0;
+
+            while (i < 50)
+            {
+                // act
+                var result = service.GetNextBet();
+
+                // then 
+                Assert.IsNotNullOrEmpty(result.ImageName);
+                i++;
+            }
+
         }
 
     }
