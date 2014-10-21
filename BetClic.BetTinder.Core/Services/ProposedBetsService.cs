7b9aeb1d5a563206e8df68d637c21cfb020aebaa
driznar <driznar+github@gmail.com>
2014-10-21 11:38:32 +0200

    - replaced image byte map property with image path property - when obtaining the next bet the image path property is being set

--- BetClic.BetTinder.Core/Services/ProposedBetsService.cs
+++ BetClic.BetTinder.Core/Services/ProposedBetsService.cs
@@ -5,13 +5,24 @@ namespace BetClic.BetTinder.Core.Services
 {
     public class ProposedBetsService : IProposedBetsService
     {
-        public Bet GetNextBet()
+        private string[] imageNames;
+        int i = 0;
+
+        public ProposedBetsService()
         {
+            imageNames = new string[]{ "pic1", "pic2", "pic3", "pic4", "pic5" };
+        }
+
+        public Bet GetNextBet()
+        {           
             return new Bet()
             {
                 Name = Guid.NewGuid().ToString().Substring(0, 6),
-                Odds = new Random().NextDouble()
+                Odds = new Random().NextDouble(),
+                ImageName = imageNames[i % 5]
             };
+
+            i++;
         }
     }
 }
